def52290d420b1654ae59c6b09a5cc266f6189f2
Gieted <gieted@protonmail.com>
2020-06-14 14:01:39 +0200

    Revert "Revert "Remove initial update check""
    This reverts commit 692e3827

--- src/app/update.service.ts
+++ src/app/update.service.ts
@@ -18,7 +18,6 @@ export class UpdateService {
   async listenForUpdate(): Promise<void> {
     let updateCheck: Subscription;
     if (this.swUpdate.isEnabled) {
-      this.checkForUpdate().catch(console.error);
       updateCheck = interval(1000 * 60 * 30).subscribe(this.checkForUpdate.bind(this));
     }
     const updateAvailable = this.swUpdate.available.subscribe(() => {
