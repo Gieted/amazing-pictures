7ada1c340cb2fa957f7bece8dc06fa861151c1a9
Gieted <gieted@protonmail.com>
2020-06-14 18:09:32 +0200

    Revert "Remove manual update checks"
    This reverts commit 5969b384

--- src/app/update.service.ts
+++ src/app/update.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { interval, Subscription } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,21 @@ export class UpdateService {
 
   constructor(private swUpdate: SwUpdate, private snackBar: MatSnackBar) { }
 
+  async checkForUpdate() {
+    if (this.swUpdate.isEnabled && !this.updateAvailable) {
+      console.log('Checking for update');
+      await this.swUpdate.checkForUpdate();
+    }
+  }
+
   async listenForUpdate(): Promise<void> {
+    let updateCheck: Subscription;
+    if (this.swUpdate.isEnabled) {
+      updateCheck = interval(1000 * 60 * 30).subscribe(this.checkForUpdate.bind(this));
+    }
     const updateAvailable = this.swUpdate.available.subscribe(() => {
       updateAvailable.unsubscribe();
+      updateCheck.unsubscribe();
       this.updateAvailable = true;
       console.log('New update available');
       const snack = this.snackBar.open('Update available', 'UPDATE NOW');
