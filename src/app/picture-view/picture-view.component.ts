98d4f4dd3055a8f310cb0c704b816be808968452
Gieted <gieted@protonmail.com>
2020-06-15 02:00:48 +0200

    Add picture info display

--- src/app/picture-view/picture-view.component.ts
+++ src/app/picture-view/picture-view.component.ts
@@ -5,6 +5,9 @@ import Picture from '../pictures/Picture';
 import { AccountService } from '../account.service';
 import { MatDialog } from '@angular/material/dialog';
 import { PictureDeleteComponent } from './picture-delete/picture-delete.component';
+import Profile from '../profile/Profile';
+import { ProfileService } from '../profile/profile.service';
+import { AngularFireStorage } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-picture-view',
@@ -12,29 +15,37 @@ import { PictureDeleteComponent } from './picture-delete/picture-delete.componen
   styleUrls: ['./picture-view.component.css']
 })
 export class PictureViewComponent implements OnInit {
+  readonly defaultProfilePictureUrl = 'assets/images/default-profile-picture.png';
+
   id: string;
   picture: Picture;
   my: boolean;
+  profile: Profile;
+  profilePicUrl: string;
 
   constructor(route: ActivatedRoute,
               private browserService: BrowserService,
               private accountService: AccountService,
               private dialog: MatDialog,
-              private router: Router) {
+              private router: Router,
+              private profileService: ProfileService,
+              private storage: AngularFireStorage) {
 
     route.params.subscribe(async params => {
       this.id = params.id;
       if (!browserService.loading) {
-        this.refresh();
+        await this.refresh();
       }
     });
   }
 
-  refresh(): void {
+  async refresh(): Promise<void> {
     this.picture = this.browserService.pictures.find(picture => picture.id === this.id);
     if (this.picture) {
       this.my = this.picture.authorId === this.accountService.user?.uid;
     }
+    this.profile = await this.profileService.getProfile(this.picture.authorId);
+    this.profilePicUrl = await this.storage.ref('users').child(this.picture.authorId).child('profile-picture').getDownloadURL().toPromise();
   }
 
   ngOnInit(): void {
