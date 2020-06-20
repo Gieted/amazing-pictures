705efcc3512fe5889cf7c0ac595cb5ea31bbbad0
Gieted <gieted@protonmail.com>
2020-06-20 12:00:36 +0200

    Fix fill parent directive bug

--- src/app/fill-parent.directive.ts
+++ src/app/fill-parent.directive.ts
@@ -17,8 +17,10 @@ export class FillParentDirective implements OnInit {
       const orientation: 'horizontal' | 'vertical' = height < width ? 'horizontal' : 'vertical';
       if (orientation === 'horizontal') {
         this.element.style.height = '100%';
+        this.element.style.width = null;
       } else {
         this.element.style.width = '100%';
+        this.element.style.height = null;
       }
       this.element.style.opacity = 1;
     });
